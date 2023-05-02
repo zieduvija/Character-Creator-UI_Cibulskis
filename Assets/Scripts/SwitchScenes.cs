2ea917a3f354120cc1c18a9de2e5fcecfd5886af
2PT-2 <118617121+zieduvija@users.noreply.github.com>
2023-05-02 10:52:11 +0300

    Izveidots projekts, izveidots sākuma ekrāns un sākts darbs uz tēla veidošanas ekrānu.

---
+++ Assets/Scripts/SwitchScenes.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SwitchScenes : MonoBehaviour {
+
+    public void Create()
+    {
+        //Loads the scene named "Creator"
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Creator");
+    }
+
+    public void Info()
+    {
+        //Loads the scene named "Info"
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Info");
+    }
+    
+    public void Exit()
+    {
+        //Loads the scene named "Exit"
+        UnityEngine.SceneManagement.SceneManager.LoadScene("Exit");
+    }
+}
